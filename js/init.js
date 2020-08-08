5c7e65fb2fd545d2a670ba038b9f8d22cccddbd9
simoaweb <simoaweb@gmail.com>
2020-08-08 13:45:36 -0300

    novobotao

--- js/init.js
+++ js/init.js
@@ -7,3 +7,20 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+paypal.Buttons({
+      style: {
+          shape: 'rect',
+          color: 'gold',
+          layout: 'vertical',
+          label: 'subscribe',
+          
+      },
+      createSubscription: function(data, actions) {
+        return actions.subscription.create({
+          'plan_id': 'P-0T2408869C774372AL4XMT7Y'
+        });
+      },
+      onApprove: function(data, actions) {
+        alert(data.subscriptionID);
+      }
+  }).render('#paypal-button-container');
